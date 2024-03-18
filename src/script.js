fc88a0e6dbadd107cae50dc3b11ca99628b98873
Kami Karras <kamikarras@gmail.com>
2024-03-18 09:38:34 -0700

    remove unused items

--- src/script.js
+++ src/script.js
@@ -1,25 +1,17 @@
 import * as THREE from 'three'
 import gsap from 'gsap'
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
-import GUI from 'lil-gui'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
 import vertexShader from './shaders/vertex.glsl'
 import fragmentShader from './shaders/fragment.glsl'
-import { LoadGLTFByPath } from './helpers/ModelHelper.js'
-import PositionAlongPathState from './positionAlongPathTools/PositionAlongPathState.js';
-import { handleScroll, updatePosition } from './positionAlongPathTools/PositionAlongPathMethods.js'
-import { loadCurveFromJSON } from './curveTools/CurveMethods.js'
-import { setupRenderer } from './helpers/RendererHelper.js'
 
-const curvePathJSON = './static/cameraP.json'
+
 
 let animating = false;
 let ended = false;
 /**
  * Base
  */
-// Debug
-// const gui = new GUI()
+
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -63,29 +55,7 @@ camera.position.set(0, 8, 30)
 camera.lookAt(0,2,0)
 scene.add(camera)
 const animateCamera = (end)=>{
-    // if(end){animating = true;
-    //     words.style.display =  "none"
-    //     gsap.to(camera.position, { duration: 3, delay: 4, z: 0 })
-    //     gsap.to(camera.rotation, { duration: 3, delay: 4.5, z: Math.PI/2 })
-    //     gsap.to(camera.rotation, { duration: 3, delay: 4.5, x: -Math.PI/2 })
-    //     gsap.to(camera.position, { duration: 4, delay: 8, y: -5 })
-    //     gsap.to(camera.position, { duration: 4, delay: 10, y: -10 })
-    //     gsap.to(camera.position, { duration: 4, delay: 14, y: -25 })
-    //     gsap.to(camera.position, { duration: 5, delay: 16, z: -35 })
-    //     gsap.to(camera.position, { duration: 5, delay: 16, y: -35 })
-    //     gsap.to(camera.rotation, { duration: 4, delay: 16, x: -Math.PI })
-    //     gsap.to(camera.position, { duration: 5, delay: 24, y: -125 })
-    //     gsap.to(camera.position, { duration: 5, delay: 24, z:0 })
-    //     gsap.to(camera.position, { duration: 5, delay: 24, x:0 })
-    //     // gsap.to(camera.rotation, { duration: 4, delay: 24, x: Math.PI })
-    //     gsap.to(camera.rotation, { duration: 4, delay: 24, x: camera.rotation.x-Math.PI - Math.PI/2 })
-    //     // gsap.to(camera.rotation, { duration: 4, delay: 18, y: -Math.PI/2 })
-    //     setTimeout(()=>{
-    //         words.innerHTML = "song: MOOO! </br> Artist: Doja Cat"
-    //         words.style.display =  "block"
-    //         animating = false
-    //         ended = true
-    //     }, 28000 )}
+
 
     animating = true;
     words.style.display =  "none"
@@ -101,26 +71,13 @@ const animateCamera = (end)=>{
     gsap.to(camera.position, { duration: 5, delay: 24, y: -125 })
     gsap.to(camera.position, { duration: 5, delay: 24, z:0 })
     gsap.to(camera.position, { duration: 5, delay: 24, x:0 })
-    // gsap.to(camera.rotation, { duration: 4, delay: 24, x: Math.PI })
     gsap.to(camera.rotation, { duration: 4, delay: 24, x: camera.rotation.x-Math.PI - Math.PI/2 })
-    // gsap.to(camera.rotation, { duration: 4, delay: 18, y: -Math.PI/2 })
     setTimeout(()=>{
         words.innerHTML = "<a href='https://open.spotify.com/track/00bWqt93aqLXqKtzZoq7Jw?si=496b1744289746e4'>song: MOOO! </br> Artist: Doja Cat</a>"
         words.style.display =  "block"
-        // animating = false
         ended = true
     }, 28000 )
 }
-// gsap.to(camera.position, { duration: 3, delay: 2, z: 0 })
-// gsap.to(camera.rotation, { duration: 3, delay: 2.5, z: Math.PI/2 })
-// gsap.to(camera.rotation, { duration: 3, delay: 2.5, x: -Math.PI/2 })
-// gsap.to(camera.position, { duration: 4, delay: 6, y: -5 })
-// gsap.to(camera.position, { duration: 4, delay: 10, y: -10 })
-
-
-// Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
 
 /**
  * Renderer
@@ -135,13 +92,6 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setClearColor(rendererParameters.clearColor)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-// gui
-//     .addColor(rendererParameters, 'clearColor')
-//     .onChange(() =>
-//     {
-//         renderer.setClearColor(rendererParameters.clearColor)
-//     })
 
 /**
  * Material
@@ -167,28 +117,12 @@ let light = new THREE.DirectionalLight(0xffffff, 2)
 /**
  * Objects
  */
-// Torus knot
-const torusKnot = new THREE.Mesh(
-    new THREE.TorusKnotGeometry(0.6, 0.25, 128, 32),
-    material
-)
-torusKnot.position.x = 3
-// scene.add(torusKnot)
 
-// Sphere
-const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry(50,50,50),
-    material
-)
-plane.rotation.x = -Math.PI /2
-// sphere.position.x = - 3
-// scene.add(plane)
 
 const plane2 = new THREE.Mesh(
     new THREE.PlaneGeometry(50,50,50),
     material
 )
-// plane2.rotation.x = -Math.PI /2
 plane2.position.z = -25
 plane2.position.y = 25
 scene.add(plane2)
@@ -248,7 +182,7 @@ audioLoader.load( './music/song.mp3', (buffer)=> {
     song.setBuffer( buffer );
     song.setLoop(true);
     song.setVolume(0.5);
-    // song.play();
+
   });
 
   window.addEventListener('click', ()=>{
@@ -263,36 +197,15 @@ audioLoader.load( './music/song.mp3', (buffer)=> {
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-let counter = 0.0;
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
 
     data = analyser.getAverageFrequency();
-
-    // Rotate objects
-    // if(wholeScene)
-    // {
-    //     wholeScene.rotation.x = - elapsedTime * 0.1
-    //     wholeScene.rotation.y = elapsedTime * 0.2
-    // }
-
-    // sphere.rotation.x = - elapsedTime * 0.1
-    // sphere.rotation.y = elapsedTime * 0.2
-
-    // torusKnot.rotation.x = - elapsedTime * 0.1
-    // torusKnot.rotation.y = elapsedTime * 0.2
-
-    // Update controls
-    // controls.update()
 
     // Render
     renderer.render(scene, camera)
     material.uniforms.uTime.value = data*.005
 
-    // camera.lookAt(0,0,0)
-    
 
     // Call tick again on the next frame
     window.requestAnimationFrame(tick)
